23c4b5d472c34d56cc376ee356232e95b67b1784
Valentin Chanel <valentin.chanel@telecomnancy.eu>
2021-12-02 16:11:06 +0100

    ajout des sessions

--- FLASK et WEB/application.py
+++ FLASK et WEB/application.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session,request,redirect
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 @app.route('/')
 def main():
@@ -25,6 +30,14 @@ def problematique(id_prob):
     pb = id_prob # avec une querry on va chercher donner de la problématique
     return render_template('problematique.html',pb = pb)
 
-@app.route('/login')
+@app.route('/login', methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        return redirect("/")
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session["name"] = None
+    return redirect('/')
