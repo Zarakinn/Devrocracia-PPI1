2db38f067f250db7d7bdda6caa64201272540328
Valentin Chanel <valentin.chanel@telecomnancy.eu>
2021-12-06 15:42:32 +0100

    ajout fonctions + affichage

--- FLASK et WEB/application.py
+++ FLASK et WEB/application.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session,request,redirect
 from flask_session import Session
+import fonctions_pratique
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
@@ -17,18 +18,15 @@ def help():
 
 @app.route('/problematiques')
 def problematiques():
-    liste_prob = [[1,'01-12-2021','Ca manque de verdure !','La ville est grise donc ajoutons de la verdure',1],
-                [2,'01-12-2021','Avancement du projet','Je pense qu on est trop fort, on avance bien',2]
-                ]
-                 # querry de type SELECT * FROM problematiques
-    # besoin de traiter les données avant de les renvoyer ?
+    liste_prob = fonctions_pratique.GetProblematiques()
     return render_template('problematiques.html',liste_prob = liste_prob,len = len(liste_prob)) 
 
 
 @app.route('/problematique/<int:id_prob>')
 def problematique(id_prob):
-    pb = id_prob # avec une querry on va chercher donner de la problématique
-    return render_template('problematique.html',pb = pb)
+
+    spbs = fonctions_pratique.GetSousProblematiques(id_prob)
+    return render_template('problematique.html',spbs = spbs)
 
 @app.route('/login', methods=["GET","POST"])
 def login():
---
+++ FLASK et WEB/fonctions_pratique.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+
+def Creation_Problemes(titre,description) -> None :
+    """calcule de la date, d’un id unique et création d’une ligne dans le schéma problématique"""
+    try:
+        connexion = sqlite3.connect("database.db")
+        cursor = connexion.cursor()
+        print("Connexion réussie à SQLite")
+        cursor.execute("SELECT date('now')")
+        date =cursor.fetchone()
+        date=date[0]
+        cursor.execute("SELECT max(id) FROM pb ")
+        res=cursor.fetchone()
+        res=res[0]+1
+        sql = "INSERT INTO pb (id,DateCreation,titre,texte) VALUES (?, ?, ?, ?)"
+        donnees=[(res,date,titre,description)]
+        cursor.executemany(sql, donnees)
+        connexion.commit()
+        connexion.commit()
+        print("Enregistrements insérés avec succès dans la table pb")
+        cursor.close()
+        connexion.close()
+        print("Connexion SQLite est fermée")
+    except sqlite3.Error as error:
+        print("Erreur lors de l'insertion dans la table pb", error)
+
+
+def Creation_Proposition(Utilisateur,Sous_probleme_id,titre,description) -> None :
+    """ajoute une proposition pour un problème"""
+    try:
+        connexion = sqlite3.connect("database.db")
+        cursor = connexion.cursor()
+        print("Connexion réussie à SQLite")
+        donnees=[(Utilisateur,Sous_probleme_id,titre,description)]
+        sql = "INSERT INTO propositions (id,sous_pb_id,titre,texte) VALUES (?, ?, ?, ?)"
+        cursor.executemany(sql, donnees)
+        connexion.commit()
+        print("Enregistrements insérés avec succès dans la table propositions")
+        cursor.close()
+        connexion.close()
+        print("Connexion SQLite est fermée")
+    except sqlite3.Error as error:
+        print("Erreur lors de l'insertion dans la table propositions", error)
+
+
+def Get_Selected_Propostion(id) -> int :
+    """cherche toutes les propositions associées aux problèmes, retourne l’id celle qui à le plus de vote"""
+    try:
+        connexion = sqlite3.connect("database.db")
+        cursor = connexion.cursor()
+        print("Connexion réussie à SQLite")
+        cursor.execute("SELECT max(nb_vote) FROM propositions ")
+        res=cursor.fetchone()
+        res=res[0]
+        print(res)
+        cursor.execute("SELECT PR.id FROM propositions PR JOIN  pb PB ON PR.id=? WHERE nb_vote=? ", (id,res,))
+        new_id=cursor.fetchone()
+        new_id=new_id[0]
+        print("Enregistrements éxécutés avec succès dans la table pb")
+        cursor.close()
+        connexion.close()
+        print("Connexion SQLite est fermée")
+        return new_id
+    except sqlite3.Error as error:
+        print("Erreur lors de l'insertion dans la table pb", error)
+
+
+print(Get_Selected_Propostion(1))
+
+
+def Etend_Branche(utilisateur,sous_problème) -> None :
+    """créé un nouveau sous problème en fonction de la proposition choisit, créé les données associé dans la base de données 
+    -> le en fonction de sous proposition à besoin d'une fonction pour savoir qui a eu le plus de vote"""
+    try:
+        connexion = sqlite3.connect("database.db")
+        cursor = connexion.cursor()
+        print("Connexion réussie à SQLite")
+        donnees=[(utilisateur, sous_problème)]
+        sql = "INSERT INTO sous_pb (id,titre) VALUES (?, ?)"
+        cursor.executemany(sql, donnees)
+        connexion.commit()
+        print("Enregistrements insérés avec succès dans la table sous_pb")
+        cursor.close()
+        connexion.close()
+        print("Connexion SQLite est fermée")
+    except sqlite3.Error as error:
+        print("Erreur lors de l'insertion dans la table sous_pb", error)
+
+
+def Vote(utilisateur, proposition) -> None :
+    """vérifie contrainte d’intégrité, éligibilité aux votes et change compte de vote"""
+    return None 
+
+
+def EnvoieMessage(utilisateur,texte,sous_proposition_id) -> None :
+    """créé une ligne dans le schéma message, l’associe aux sous problème"""
+    try:
+        connexion = sqlite3.connect('database.db')
+        cursor = connexion.cursor()
+        print("Connexion réussie à SQLite")
+        cursor.execute("SELECT max(id) FROM message ")
+        res=cursor.fetchone()
+        res=res[0]+1
+        sql = "INSERT INTO message (id,texte,utilisateur_id,sous_pb_id) VALUES (?, ?, ?, ?)"
+        donnes=[(res,texte,utilisateur,sous_proposition_id)]
+        cursor.executemany(sql,donnes )
+        connexion.commit()
+        print("Enregistrements insérés avec succès dans la table message")
+        cursor.close()
+        connexion.close()
+        print("Connexion SQLite est fermée")
+    except sqlite3.Error as error:
+        print("Erreur lors de l'insertion dans la table message/sous_pb", error)
+
+
+## Récupération de données à afficher
+
+def GetProblematiques() -> list:
+    try:
+        connexion = sqlite3.connect('database.db')
+        cursor = connexion.cursor()
+        print("Connexion réussie à SQLite")
+        sql_request = "SELECT * FROM pb"
+        cursor.execute(sql_request )
+        pbs = cursor.fetchall()
+        print("Récupération des problématiques réussi")
+        cursor.close()
+        connexion.close()
+        print("Connexion SQLite est fermée")
+        return pbs
+    except sqlite3.Error as error:
+        print("Erreur lors de la récupération des problématiques", error)
+
+def GetSousProblematiques(id_prob : int) -> list:
+    try:
+        connexion = sqlite3.connect('database.db')
+        cursor = connexion.cursor()
+        print("Connexion réussie à SQLite")
+
+        liste_spbs = []
+
+        cursor.execute("SELECT * FROM sous_pb WHERE pb_parent_id = ? AND id=sous_pb_parent_id ",(id_prob))
+        spb = cursor.fetchone()
+        liste_spbs.append(spb)
+
+        b = True
+        while b:
+            cursor.execute("SELECT * FROM sous_pb WHERE sous_pb_parent_id = ?", liste_spbs[len(liste_spbs)][4]) # on cherche si la derniere sous proposition a un enfant
+            spb = cursor.fetchone()
+            if spb == None:
+                b = False
+                break
+            else :
+                liste_spbs.append(spb)
+
+
+        print("Récupération des sous_problématiques réussi")
+        cursor.close()
+        connexion.close()
+        print("Connexion SQLite est fermée")
+        return 
+    except sqlite3.Error as error:
+        print("Erreur lors de la récupération des sous-problématiques", error)
